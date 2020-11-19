b721d7e72ba4c755299017389864c1d078412a76
whh97 <1727336114@qq.com>
2020-11-19 20:48:03 +0800

    dfs

--- javascript/js实战/01.js
+++ javascript/js实战/01.js
@@ -1,34 +1,18 @@
-/*var twoSum = function(nums, target) {
-	let map = new Map();
-	for(let i in nums){
-	    map.set(
-	        nums[i],
-	        i
-	    )
-	}
+const deserialize = (data) => {
+  const list = data.split(","); // 按逗号split成数组
 
-	for(let j = 0; j < nums.length - 1; j++){
-		// 判断map中是否包含target-nums[j]作为value值，并且这个值和当前的数不是一个数
-	    if(map.has( target - nums[j] ) && map.get( target - nums[j]) != j ){
-	        return [j, map.get( target - nums[j])];
-	    }
-	}
-}*/
+  const buildTree = (list) => {
+    // dfs函数，构建当前子树
+    const rootVal = list.shift(); // 弹出首项，获取它的“数据”
+    if (rootVal == "X") {
+      // 是X，返回null节点
+      return null;
+    }
+    const root = new TreeNode(rootVal); // 不是X，则创建节点
+    root.left = buildTree(list); // 递归构建左子树
+    root.right = buildTree(list); // 递归构建右子树
+    return root; // 返回当前构建好的子树
+  };
 
-
-// 没有理解
-var twoSum = function(nums, target) {
-	   const map = {}
-	   for (let i = 0; i < nums.length; i++){
-	   	console.log(map[target - nums[i] ]);
-	       if(map[target - nums[i] ] >= 0){
-	       	console.log('结束啦')
-	           return [ map[target - nums[i] ], i]
-	       }
-	       map[nums[i]] = i;    
-	       console.log(map)        
-	    }
-    
-	}
-var arr = twoSum([2,7,11,15], 9);
-console.log(arr)
+  return buildTree(list); // 构建的入口
+};
